d3cd0258e2dda93b171d2407626c89fbb68ee753
syuh23 <seonghee5070@gmail.com>
2025-03-12 10:54:17 +0900

    add exception file writing logic

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ print('Hello Mars')
 # 문제 풀이
 try:
     file = open('./mission_computer_main.log', 'r', encoding='UTF-8')
-except FileNotFoundError:
-    print('해당 파일이 존재하지 않습니다.')
+    logList = file.readlines()
+    file.close()
 
-logList = file.readlines()
-file.close()
+    # 로그 출력
+    for log in logList:
+        print(log)
+        
+    print('\n--------------------------------------------------------\n\n')
 
-# 로그 출력
-for log in logList:
-    print(log)
+    # 로그 거꾸로 출력 (보너스 문제)
+    logList.reverse()
 
-print('\n--------------------------------------------------------\n\n')
+    for log in logList:
+        print(log)
 
-# 로그 거꾸로 출력 (보너스 문제)
-logList.reverse()
-
-for log in logList:
-    print(log)
+except Exception as e:
+    # 오류 문구 write
+    error_message = str(e)
+    error_file = open('./error_file.txt', 'w+')
+    error_file.write(error_message)
+    error_file.close()
