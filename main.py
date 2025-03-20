e4d426ce305afcdbc45f8e7e9c2d8938cba650bc
syuh23 <seonghee5070@gmail.com>
2025-03-20 15:13:34 +0900

    1-2. add code

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
-# Hello Mars 출력
-print('Hello Mars')
-
-# 문제 풀이
-try:
-    file = open('./mission_computer_main.log', 'r', encoding='UTF-8')
+# mission_computer_main.log 파일을 읽어들여서 출력
+with open('./mission_computer_main.log', 'r') as file:
     logList = file.readlines()
-    file.close()
+    
+splitLogList = []
 
-    # 로그 출력
-    for log in logList:
-        print(log, end='')
+for log in logList:
+    splitLog = log.strip().split(',')
+    splitLogList.append(splitLog)
+
+
+# 리스트 객체를 화면에 출력
+for log in splitLogList:
+    print(log)
+    
+print('\n-------------------------------------------------------------------\n')
+
+
+# 리스트 객체를 시간의 역순으로 정렬(sort)
+splitLogList.sort(reverse=True)
+
+for log in splitLogList:
+    print(log)
+
+print('\n-------------------------------------------------------------------\n')
+
+
+# 리스트 객체를 사전(Dict) 객체로 전환
+keys = ["Timestamp", "Event", "Message"]
+logDict = [dict(zip(keys, data)) for data in splitLogList]
+
+
+# dict로 변환된 log data를 json 타입으로 다듬기
+stringData = '['
+
+for log in logDict:
+    
+    stringData += '\n{'
+    
+    for key, value in log.items():
+        keyData = '"' + key + '"'
+        valueData = '"' + value + '"'
         
-    print('\n--------------------------------------------------------\n')
+        stringData += '\n \t' + keyData + ' : ' + valueData + ','
+        
+    stringData += '}, '
+stringData += ']'
 
-    # 로그 거꾸로 출력 (보너스 문제)
-    logList.reverse()
+data = stringData.replace(',},', '\n},').replace(', ]', '\n]')
 
-    for log in logList:
-        print(log, end='')
 
-except Exception as e:
-    # 오류 문구 write
-    error_message = str(e)
-    error_file = open('./error_file.txt', 'w+')
-    error_file.write(error_message)
-    error_file.close()
+# JSON 파일 포맷으로 저장
+with(open('./mission_computer_main.json', 'w') as file):
+    file.write(data)
