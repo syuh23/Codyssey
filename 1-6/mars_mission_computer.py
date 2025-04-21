cf15b514d5bc185d330b0f2ff033da4f4cccca33
syuh23 <seonghee5070@gmail.com>
2025-04-21 14:16:05 +0900

    1-6. add code

---
+++ 1-6/mars_mission_computer.py
@@ -0,0 +1,31 @@
+import random
+
+class DummySensor:
+    def __init__(self):
+        self.env_values = {
+            'mars_base_internal_temperature': None,
+            'mars_base_external_temperature': None,
+            'mars_base_internal_humidity': None,
+            'mars_base_external_illuminance': None,
+            'mars_base_internal_co2': None,
+            'mars_base_internal_oxygen': None
+        }
+
+    def set_env(self):
+        self.env_values['mars_base_internal_temperature'] = random.uniform(18, 30)
+        self.env_values['mars_base_external_temperature'] = random.uniform(0, 21)
+        self.env_values['mars_base_internal_humidity'] = random.uniform(50, 60)
+        self.env_values['mars_base_external_illuminance'] = random.uniform(500, 715)
+        self.env_values['mars_base_internal_co2'] = random.uniform(0.02, 0.1)
+        self.env_values['mars_base_internal_oxygen'] = random.uniform(4, 7)
+
+    def get_env(self):
+        return self.env_values
+
+ds = DummySensor()
+
+ds.set_env()
+env_data = ds.get_env()
+
+for key, value in env_data.items():
+    print(f'{key}: {value:.2f}')
